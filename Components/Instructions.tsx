9c41a3b45224e5a0c7308735ae59254fd6357ff0
Dominik <domi.heller@gmail.com>
2022-01-24 18:57:33 +0100

    added header

--- Components/Instructions.tsx
+++ Components/Instructions.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 import styles from "../styles/Instructions.module.css";
+import BackButton from "./BackButton";
 
 interface InstructionProps {
     handleClick: () => void;
@@ -8,30 +9,10 @@ interface InstructionProps {
 const Instructions = ({ handleClick }: InstructionProps) => {
     return (
         <div className={styles.instructions} onClick={handleClick}>
-            <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <line
-                    x1="0.646447"
-                    y1="18.7886"
-                    x2="18.3241"
-                    y2="1.11091"
-                    stroke="white"
-                    strokeOpacity="0.9"
-                />
-                <line
-                    x1="1.35355"
-                    y1="0.646447"
-                    x2="19.0312"
-                    y2="18.3241"
-                    stroke="white"
-                    strokeOpacity="0.9"
-                />
-            </svg>
+            <div>
+                <BackButton />
+                <h1 className={styles.header}>Anleitung</h1>
+            </div>
             <div className={styles.text}>
                 Das gesuchte Wort besteht aus fünf Buchstaben. Errate es in
                 sechs Versuchen. Jeder Rateversuch bringt neue Hinweise,
