d3e6ea5b586afcf474d70a403d3421a06ef8905b
Dominik <domi.heller@gmail.com>
2022-01-24 16:20:08 +0100

    added head and meta information

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import ScoreButton from "../Components/ScoreButton";
 import WordGame, { LetterCell } from "../Components/WordGame";
 import styles from "../styles/Home.module.css";
 import Cookies from "universal-cookie";
+import Head from "next/head";
 
 const Home = () => {
     const [instructionsActive, setInstructionsActive] = useState(false);
@@ -30,25 +31,37 @@ const Home = () => {
     };
 
     return (
-        <div className={styles.home}>
-            <div className={styles.headerSection}>
-                <div
-                    className={styles.menuButton}
-                    onClick={() => setInstructionsActive((prev) => !prev)}
-                >
-                    <InstructionButton />
-                </div>
-                <h1 className={styles.header}>Wortspiel</h1>
-                <div className={styles.menuButton}>
-                    <ScoreButton />
-                </div>
-            </div>
-            <WordGame saveRound={saveRound} />
-            {instructionsActive && (
-                <Instructions
-                    handleClick={() => setInstructionsActive(false)}
+        <div>
+            <Head>
+                <title>Wortspiel</title>
+                <meta charSet="utf-8" />
+                <meta name="author" content="KilianMandscharo" />
+                <meta
+                    name="description"
+                    content="Deutsche Version des Spiels WORDLE. Errate das gesuchte Wort in sechs Versuchen."
                 />
-            )}
+                <meta name="keywords" content="wordle, deutsch, wortspiel" />
+            </Head>
+            <div className={styles.home}>
+                <div className={styles.headerSection}>
+                    <div
+                        className={styles.menuButton}
+                        onClick={() => setInstructionsActive((prev) => !prev)}
+                    >
+                        <InstructionButton />
+                    </div>
+                    <h1 className={styles.header}>Wortspiel</h1>
+                    <div className={styles.menuButton}>
+                        <ScoreButton />
+                    </div>
+                </div>
+                <WordGame saveRound={saveRound} />
+                {instructionsActive && (
+                    <Instructions
+                        handleClick={() => setInstructionsActive(false)}
+                    />
+                )}
+            </div>
         </div>
     );
 };
