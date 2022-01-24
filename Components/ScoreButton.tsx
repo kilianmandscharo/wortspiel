caf40eec517a36added9f686119ebd3b2277c4bb
Dominik <domi.heller@gmail.com>
2022-01-24 14:19:55 +0100

    put the svgs in extra components

---
+++ Components/ScoreButton.tsx
@@ -0,0 +1,38 @@
+const ScoreButton = () => {
+    return (
+        <svg
+            width="22"
+            height="25"
+            viewBox="0 0 22 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect
+                x="0.75"
+                y="7.75"
+                width="6.5"
+                height="16.5"
+                stroke="rgba(255, 255, 255, 0.9)"
+                strokeWidth="1.5"
+            />
+            <rect
+                x="14.75"
+                y="11.75"
+                width="6.5"
+                height="12.5"
+                stroke="rgba(255, 255, 255, 0.9)"
+                strokeWidth="1.5"
+            />
+            <rect
+                x="7.75"
+                y="0.75"
+                width="6.5"
+                height="23.5"
+                stroke="rgba(255, 255, 255, 0.9)"
+                strokeWidth="1.5"
+            />
+        </svg>
+    );
+};
+
+export default ScoreButton;
