ffdfb269f21e377468d5b9bbc0712681ac10a97b
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-03 19:11:21 +0100

    added media queries

--- Components/Scores.tsx
+++ Components/Scores.tsx
@@ -31,34 +31,36 @@ const Scores = ({
                     <BackButton />
                     <h1 className={styles.header}>Scores</h1>
                 </div>
-                <div className={styles.barChart}>
-                    {Object.entries(guessesForEachNumber).map(
-                        ([key, value]: [string, any]) => (
-                            <div
-                                key={key}
-                                className={styles.bar}
-                                style={{
-                                    width: `${20 + value * ratio}px`,
-                                }}
-                            >
-                                <div className={styles.number}>{key}</div>
-                                <div className={styles.guess}>{value}</div>
-                            </div>
-                        )
-                    )}
-                </div>
-                <div className={styles.stats}>
-                    <div className={styles.statEntry}>
-                        Spiele: {gamesPlayed}
+                <div className={styles.content}>
+                    <div className={styles.barChart}>
+                        {Object.entries(guessesForEachNumber).map(
+                            ([key, value]: [string, any]) => (
+                                <div
+                                    key={key}
+                                    className={styles.bar}
+                                    style={{
+                                        width: `${20 + value * ratio}px`,
+                                    }}
+                                >
+                                    <div className={styles.number}>{key}</div>
+                                    <div className={styles.guess}>{value}</div>
+                                </div>
+                            )
+                        )}
                     </div>
-                    <div className={styles.statEntry}>
-                        Höchste Gewinnserie: {highestStreak}
-                    </div>
-                    <div className={styles.statEntry}>
-                        Aktuelle Gewinnserie: {currentStreak}
-                    </div>
-                    <div className={styles.statEntry}>
-                        Gewinnanteil: {winPercentage}%
+                    <div className={styles.stats}>
+                        <div className={styles.statEntry}>
+                            Spiele: {gamesPlayed}
+                        </div>
+                        <div className={styles.statEntry}>
+                            Höchste Gewinnserie: {highestStreak}
+                        </div>
+                        <div className={styles.statEntry}>
+                            Aktuelle Gewinnserie: {currentStreak}
+                        </div>
+                        <div className={styles.statEntry}>
+                            Gewinnanteil: {winPercentage}%
+                        </div>
                     </div>
                 </div>
             </div>
