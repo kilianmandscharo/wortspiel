a628d6e4fe8c526c50fcda76d38d0fdbc5f4cdb9
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-14 15:32:25 +0100

    cleaned up css wordGame

--- functions/determineClassForLetter.ts
+++ functions/determineClassForLetter.ts
@@ -24,53 +24,53 @@ const determineClassForLetter = (
     } else {
         if (letterCell.status === Status.correctPositon) {
             if (row === animationRowNumber && col === 0) {
-                return `${styles.letter} ${styles.zeroGreen}`;
+                return `${styles.letter} ${styles.greenAnimation} ${styles.zero}`;
             }
             if (row === animationRowNumber && col === 1) {
-                return `${styles.letter} ${styles.oneGreen}`;
+                return `${styles.letter} ${styles.greenAnimation} ${styles.one}`;
             }
             if (row === animationRowNumber && col === 2) {
-                return `${styles.letter} ${styles.twoGreen}`;
+                return `${styles.letter} ${styles.greenAnimation} ${styles.two}`;
             }
             if (row === animationRowNumber && col === 3) {
-                return `${styles.letter} ${styles.threeGreen}`;
+                return `${styles.letter} ${styles.greenAnimation} ${styles.three}`;
             }
             if (row === animationRowNumber && col === 4) {
-                return `${styles.letter} ${styles.fourGreen}`;
+                return `${styles.letter} ${styles.greenAnimation} ${styles.four}`;
             }
         }
         if (letterCell.status === Status.correctLetter) {
             if (row === animationRowNumber && col === 0) {
-                return `${styles.letter} ${styles.zeroOrange}`;
+                return `${styles.letter} ${styles.orangeAnimation} ${styles.zero}`;
             }
             if (row === animationRowNumber && col === 1) {
-                return `${styles.letter} ${styles.oneOrange}`;
+                return `${styles.letter} ${styles.orangeAnimation} ${styles.one}`;
             }
             if (row === animationRowNumber && col === 2) {
-                return `${styles.letter} ${styles.twoOrange}`;
+                return `${styles.letter} ${styles.orangeAnimation} ${styles.two}`;
             }
             if (row === animationRowNumber && col === 3) {
-                return `${styles.letter} ${styles.threeOrange}`;
+                return `${styles.letter} ${styles.orangeAnimation} ${styles.three}`;
             }
             if (row === animationRowNumber && col === 4) {
-                return `${styles.letter} ${styles.fourOrange}`;
+                return `${styles.letter} ${styles.orangeAnimation} ${styles.four}`;
             }
         }
         if (letterCell.status === Status.false) {
             if (row === animationRowNumber && col === 0) {
-                return `${styles.letter} ${styles.zeroBlack}`;
+                return `${styles.letter} ${styles.blackAnimation} ${styles.zero}`;
             }
             if (row === animationRowNumber && col === 1) {
-                return `${styles.letter} ${styles.oneBlack}`;
+                return `${styles.letter} ${styles.blackAnimation} ${styles.one}`;
             }
             if (row === animationRowNumber && col === 2) {
-                return `${styles.letter} ${styles.twoBlack}`;
+                return `${styles.letter} ${styles.blackAnimation} ${styles.two}`;
             }
             if (row === animationRowNumber && col === 3) {
-                return `${styles.letter} ${styles.threeBlack}`;
+                return `${styles.letter} ${styles.blackAnimation} ${styles.three}`;
             }
             if (row === animationRowNumber && col === 4) {
-                return `${styles.letter} ${styles.fourBlack}`;
+                return `${styles.letter} ${styles.blackAnimation} ${styles.four}`;
             }
         }
     }
